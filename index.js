ad5040789d107196f8d2ea9cc375bdfc406f9a62
carlossdr03s <carlossodreibm@gmail.com>
2023-09-26 17:23:02 -0300

    feat: third text working

--- index.js
+++ index.js
@@ -3,8 +3,20 @@ function firstNonConsecutive(arr) {
     return null;
   }
 
+  for (let i = 0; i < arr.length; i++) {
+    const firstNumber = arr[i];
+    const secondNumber = arr[i + 1] === undefined ? arr[arr.length -1] : arr[i + 1];
+
+    if (firstNumber + 1 !== secondNumber) {
+      return firstNumber + 1;
+    }
+  }
+
   return arr;
 }
+
+console.log(firstNonConsecutive([1, 2, 3, 4, 5, 7]));
+ 
 
 module.exports = firstNonConsecutive;
  
--- index.test.js
+++ index.test.js
@@ -8,9 +8,9 @@ test('passes a single value', () => {
   expect(firstNonConsecutive([1])).toEqual(null);
 });
 
-// test('passes an list of of consecutive numbers until 4, then skips to 6 until 8', () => {
-//   expect(firstNonConsecutive([1, 2, 3, 4, 6, 7, 8])).toEqual(6);
-// });
+test('passes an list of consecutive numbers until 4, then skips to 6 until 8', () => {
+  expect(firstNonConsecutive([1, 2, 3, 4, 6, 7, 8])).toEqual(5);
+});
 
 // test('passes an list of consecutive numbers until 8', () => {
 //   expect(firstNonConsecutive([1, 2, 3, 4, 5, 6, 7, 8])).toEqual(null);
