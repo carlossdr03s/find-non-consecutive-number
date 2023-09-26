e2347f9cb0b995728b152ad9dd2092cf58fc4061
carlossdr03s <carlossodreibm@gmail.com>
2023-09-26 16:56:20 -0300

    chore: 2 tests updated

--- index.test.js
+++ index.test.js
@@ -4,9 +4,9 @@ test('passes an empty array', () => {
     expect(firstNonConsecutive([])).toEqual(null);
 });
 
-// test('passes an empty array', () => {
-//   expect(firstNonConsecutive([])).toEqual(null);
-// });
+test('passes a single value', () => {
+  expect(firstNonConsecutive([1])).toEqual(null);
+});
 
 // test('passes an list of of consecutive numbers until 4, then skips to 6 until 8', () => {
 //   expect(firstNonConsecutive([1, 2, 3, 4, 6, 7, 8])).toEqual(6);
