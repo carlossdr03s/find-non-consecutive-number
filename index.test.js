ff5db7a871de9c05698e292db071fd7cef8588c8
carlossdr03s <carlossodreibm@gmail.com>
2023-09-26 16:53:56 -0300

    chore: commenting tests

--- index.test.js
+++ index.test.js
@@ -4,22 +4,19 @@ test('passes an empty array', () => {
     expect(firstNonConsecutive([])).toEqual(null);
 });
 
-test('passes an empty array', () => {
-  expect(firstNonConsecutive([])).toEqual(null);
-});
-
-test('passes an list of of consecutive numbers until 4, then skips to 6 until 8', () => {
-  expect(firstNonConsecutive([1, 2, 3, 4, 6, 7, 8])).toEqual(6);
-});
-
-test('passes an list of consecutive numbers until 8', () => {
-  expect(firstNonConsecutive([1, 2, 3, 4, 5, 6, 7, 8])).toEqual(null);
-});
-
-test('passes an list of string', () => {
-  expect(firstNonConsecutive('hello world')).toEqual(expect.any(Number));
-});
-
-// test('passes an list of descending numbers order', () => {
-//   expect(firstNonConsecutive([5, 4, 3, 2 ,1])).toEqual(expect.any(Number));
+// test('passes an empty array', () => {
+//   expect(firstNonConsecutive([])).toEqual(null);
 // });
+
+// test('passes an list of of consecutive numbers until 4, then skips to 6 until 8', () => {
+//   expect(firstNonConsecutive([1, 2, 3, 4, 6, 7, 8])).toEqual(6);
+// });
+
+// test('passes an list of consecutive numbers until 8', () => {
+//   expect(firstNonConsecutive([1, 2, 3, 4, 5, 6, 7, 8])).toEqual(null);
+// });
+
+// test('passes an list of string', () => {
+//   expect(firstNonConsecutive('hello world')).toEqual(expect.any(Number));
+// });
+
